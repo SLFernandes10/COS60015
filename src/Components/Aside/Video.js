7b48179bde15b41454563a747b1eae975817cef4
SLFernandes10 <164002742+SLFernandes10@users.noreply.github.com>
2024-06-23 22:32:45 +1000

    type in parameter for video

--- src/Components/Aside/Video.js
+++ src/Components/Aside/Video.js
@@ -5,7 +5,7 @@ function Video({youtubeID}){
 return(
     <iframe className='video'
         title='Youtube player' 
-        referrerpolicy='no-referrer-when-downgrade'
+        referrerPolicy='no-referrer-when-downgrade'
         sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
         src={`https://youtube.com/embed/${youtubeID}?autoplay=0`}>
     </iframe>
