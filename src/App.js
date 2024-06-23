ce2ac77b6f180522849ad9e2caf4c8aa632713ed
SLFernandes10 <164002742+SLFernandes10@users.noreply.github.com>
2024-06-23 23:37:05 +1000

    Error boundary and automated testing setups

--- src/App.js
+++ src/App.js
@@ -6,16 +6,45 @@ import Main from "./Components/Main/Main";
 import Aside from "./Components/Aside/Aside";
 import Footer from "./Components/Footer/Footer";
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
+
 function App() {
   return (
     <div className="App">
         <Header />
         <div className="grid_container">
-          <Aside />
-          <Main />
-          <Navigation />
+          <ErrorBoundary>
+            <Aside />
+          </ErrorBoundary>
+          <ErrorBoundary>
+            <Main />
+          </ErrorBoundary>
+          <ErrorBoundary>
+            <Navigation />
+          </ErrorBoundary>
         </div>
-        <Footer />
+        <ErrorBoundary>
+          <Footer />
+        </ErrorBoundary>
         </div>
   );
 }
