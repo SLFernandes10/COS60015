a021be798c55b341f88f532be953f5cc18b56a6d
SLFernandes10 <164002742+SLFernandes10@users.noreply.github.com>
2024-06-23 22:32:28 +1000

    Added error handling for email field

--- src/Components/Footer/Newsletter.js
+++ src/Components/Footer/Newsletter.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { EmailField } from "react";
+
 
 function Newsletter() {
 
@@ -15,8 +16,15 @@ function Newsletter() {
   }
 
   function confirmed (event) {
-    alert('Thank you for signing up!');
-    closeRegForm ();
+    var validRegex = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
+    let input = document.getElementById("userInput").value;
+    if (input.match(validRegex)) {
+      alert('Thank you for signing up!');
+      closeRegForm();
+    } else {
+      alert('Invalid email entry. Please enter a valid email address.')
+      openRegForm();
+    }
   }
 
   return (
@@ -26,7 +34,7 @@ function Newsletter() {
         <div class="form" id="pop-up-form">
             <h1>Newsletter Sign-Up</h1>
             <label for="email"><b>Email Address</b></label>
-            <input type="text" placeholder="Enter Email" name="email" required />
+            <input type="email" id="userInput" placeholder="Enter Email" name="email" required />
             <button type="submit" id="btn" onClick={confirmed}>Sign-Up!</button>
             <button type="button" id="btn-cancel" onClick={closeRegForm}>Cancel</button>
         </div>
